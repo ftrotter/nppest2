e7169aab66c2a6f1748ef0e9efbb7c2dcbec0a5d
Fred Trotter <fred.trotter@gmail.com>
2025-04-09 16:07:02 -0400

    calculate students for more than 6 years

--- post_import_sql/nppes_npidata_21f8c89f23754346123596e3f6c66417/20_should_be_no_forever_students.sql
+++ post_import_sql/nppes_npidata_21f8c89f23754346123596e3f6c66417/20_should_be_no_forever_students.sql
@@ -9,4 +9,11 @@ AND npi NOT IN (
   FROM taxonomies_normalized
   WHERE Healthcare_Provider_Taxonomy_Code != '390200000X'
 )
-
+;
+CREATE TABLE should_be_no_forever_students AS
+SELECT study_only_students.npi 
+FROM study_only_students
+JOIN npidata ON 
+  npidata.npi = study_only_students.npi
+WHERE npidata.Provider_Enumeration_Date > -- greater than six years ago
+  DATE_SUB(CURDATE(), INTERVAL 6 YEAR)
